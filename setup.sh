85e7423d919ee86d29c4596d2a68360ab2b01c62
joshua_jebaraj <joshuaJEBARAJ.Z@GMAIL.COM>
2021-07-25 18:42:44 +0530

    Update and rename setup.sh to test.py

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/JOSHUAJEBARAJ/markimg/raw/main/ttyd 
-
-chmod +x ttyd 
-
-nohup sudo ./ttyd -p 80 bash 
---
+++ test.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from jinja2 import Environment
+
+app = Flask(__name__)
+Jinja2 = Environment()
+
+@app.route("/page")
+def page():
+
+    name = request.values.get('name')
+    
+    # SSTI VULNERABILITY
+    # The vulnerability is introduced concatenating the
+    # user-provided `name` variable to the template string.
+    output = Jinja2.from_string('Hello ' + name + '!').render()
+    
+    # Instead, the variable should be passed to the template context.
+    # Jinja2.from_string('Hello {{name}}!').render(name = name)
+
+    return output
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
