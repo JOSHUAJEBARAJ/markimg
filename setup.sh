d965c900b07fa5d2e098bdabb99134c35bd2f0f5
Joshua <joshua@Joshuas-MacBook-Pro.local>
2021-07-26 23:48:46 +0530

    remove unwanted files

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt update 
-sudo apt install python3-pip -y
-sudo pip3 install flask
-curl https://raw.githubusercontent.com/JOSHUAJEBARAJ/markimg/main/test.py -o test.py
-sudo python3 test.py
